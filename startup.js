9091252e41252163fffce2a2ff15f54d1dafe3cf
rugeby <112892818+rugeby@users.noreply.github.com>
2023-04-05 23:29:25 -0600

    update login files

---
+++ startup.js
@@ -0,0 +1,26 @@
+//arrays + object:
+ 
+const shoppingCart = [
+    {
+       product: "cupcake1",
+       price: 3.99,
+       quantity:1,
+
+    },
+
+    {
+        product: "cupcake2",
+       price: 5.99,
+       quantity:3,
+
+    },
+
+    {
+        product: "cupcake3",
+       price: 4.99,
+       quantity:1,
+
+    }
+
+
+]
